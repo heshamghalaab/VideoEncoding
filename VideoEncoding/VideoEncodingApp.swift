a45066da8751c86a4c9d62c8788957d6c7983af9
heshamghalaab <hesham_ghalaab@hotmail.com>
2021-07-23 10:08:17 +0200

    first commit

---
+++ VideoEncoding/VideoEncodingApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoEncodingApp.swift
+//  VideoEncoding
+//
+//  Created by Ghalaab on 23/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct VideoEncodingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
